40974df96bd329cc13cd6d116117630ccf0e2c9f
hamster624 <viktors.jirgen@gmail.com>
2024-12-27 13:01:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -405,39 +405,39 @@ function showDeviceModal() {
     const statsOverlay = document.getElementById("stats-overlay");
     if (statsOverlay) {
         statsOverlay.style.position = 'absolute';
-        statsOverlay.style.right = '-250px';
+        statsOverlay.style.right = '-150px';
     }
     
     const minigamesButton = document.getElementById("openMenuButton");
     if (minigamesButton) {
         minigamesButton.style.position = 'absolute';
-        minigamesButton.style.left = '-250px';
+        minigamesButton.style.left = '-150px';
     }
     const savebutton = document.getElementById("SaveButton");
     if (savebutton) {
         savebutton.style.position = 'absolute';
-        savebutton.style.left = '-250px';
+        savebutton.style.left = '-150px';
     }
     const loadButton = document.getElementById("loadButton");
     if (loadButton) {
         loadButton.style.position = 'absolute';
-        loadButton.style.left = '-250px';
+        loadButton.style.left = '-150px';
     }
     const transcend = document.getElementById("transcendButton");
     if (transcend) {
         transcend.style.position = 'absolute';
-        transcend.style.top = '1250px';
+        transcend.style.top = '1450px';
     }
     
     const rebirth = document.getElementById("rebirthButton");
     if (rebirth) {
         rebirth.style.position = 'absolute';
-        rebirth.style.top = '1300px';
+        rebirth.style.top = '1500px';
     }    
     const calculator = document.getElementById("calculatorButton");
     if (calculator) {
         calculator.style.position = 'absolute';
-        calculator.style.left = '-250px';
+        calculator.style.left = '-150px';
     }
 }
 
